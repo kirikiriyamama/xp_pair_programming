8120f093406c3c88b0c084ba012c9a9faab74d52
Kuniaki IGARASHI <igaiga@gmail.com>
2015-08-07 21:21:18 +0900

    Calc with dicount rate

--- calculator.rb
+++ calculator.rb
@@ -6,7 +6,7 @@ class Calculator
   end
 
   def display
-    puts @price * @quantity * tax_rate
+    puts raw_price * discount_rate * tax_rate
   end
 
   def raw_price
@@ -29,5 +29,19 @@ class Calculator
   end
 
   def discount_rate
+    case raw_price
+    when 0..999
+      1.00
+    when 1000..4999
+      1.00 - 0.03
+    when 5000..6999
+      1.00 - 0.05
+    when 7000..9999
+      1.00 - 0.07
+    when 10000..49999
+      1.00 - 0.10
+    else
+      1.00 - 0.15
+    end
   end
 end
