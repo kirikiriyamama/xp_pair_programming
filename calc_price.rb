d4169f6e5476f0f12eaf7e4835e0905997ba25dc
Kuniaki IGARASHI <igaiga@gmail.com>
2015-08-07 20:53:00 +0900

    WIP spec

--- calc_price.rb
+++ calc_price.rb
@@ -1,7 +1,15 @@
-price    = ARGV[0].to_i
-quantity = ARGV[1].to_i
-area     = ARGV[2]
+class Calculator
+  def initialize
+    @price    = ARGV[0].to_i
+    @quantity = ARGV[1].to_i
+    @area     = ARGV[2]
+  end
 
-puts price * quantity * 1.0685
+  def display
+    puts @price * @quantity * 1.0685
+  end
+end
+
+Calculator.new.display
 
 
